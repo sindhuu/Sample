ea9109fd15b3436e5f5f559fd4aa3aef30b06457
vinothkumarc <vinothkumarc@PC1421.itp.objectfrontier.com>
2013-10-15 12:07:37 +0530

    Do some changes

---
+++ Servlet111/src/SampleFirst.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SampleFirst
+ */
+//@WebServlet("/SampleFirst")
+public class SampleFirst extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private PrintWriter out;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	public void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		out = response.getWriter();
+		out.println("Hello world");
+
+	}
+	
+	public void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+	
+	
+}
